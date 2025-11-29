0a84ce3cf61dec1bcdb5fe75383b1311b1e6f4cc
igor-fel1x <igor.cardosol@sptech.school>
2025-11-29 14:00:38 -0300

    alterações no css

--- web-data-viz-main/public/js/jogo.js
+++ web-data-viz-main/public/js/jogo.js
@@ -1,11 +1,10 @@
 var barra = 0
-var maxBarra = 100
+var maxBarra = 80
 var tempo = 5
 var intervalo
 var jogo = false;
 var forcaFinal = 0 
 var intervaloDiminuir
-var contagemRegressiva
 
 // Elementos do HTML
 var barraForca = document.getElementById("barraForca");
@@ -13,45 +12,25 @@ var geral = document.getElementById("geral");
 var contador = document.getElementById("contadorForca");
 var telaResultado = document.getElementById("telaResultado");
 var forcaValor = document.getElementById("forcaValor");
-var telaContagem = document.getElementById("telaContagem");
-var numeroContagem = document.getElementById("numeroContagem");
-var resultadoConteudo = document.querySelector(".resultado-conteudo");
+
 
 janela.style.display = 'none';
 telaResultado.style.display = 'none';
-telaContagem.style.display = 'none';
 b_usuario.innerHTML = sessionStorage.NOME_USUARIO;
 
-
-
-function iniciarContagem() {
-    telaContagem.style.display = "flex";
-    var contadorNumero = 3;
-    numeroContagem.innerHTML = contadorNumero;
-    
-    contagemRegressiva = setInterval(function() {
-        contadorNumero--;
-        numeroContagem.innerHTML = contadorNumero;
-        
-        if (contadorNumero <= 0) {
-            clearInterval(contagemRegressiva);
-            telaContagem.style.display = "none";
-            iniciar();
-        }
-    }, 1000);
-}
-
+contador.innerHTML = "Clique para começar";
+contador.style.backgroundColor = "#0004f1ff";
 
 function atualizar(){
-    var porc = (barra/maxBarra) * 100 
+    var porc = (barra/maxBarra) * 100;
     barraForca.style.width = porc + "%";
 
     if(porc >= 0 && porc < 40){
-        barraForca.style.backgroundColor = "#18d81eff"
-    } else if ( porc >= 40 && porc < 80){
-        barraForca.style.backgroundColor = "#fffb00ff"
+        barraForca.style.backgroundColor = "#18d81eff";
+    } else if (porc >= 40 && porc < 80){
+        barraForca.style.backgroundColor = "#fffb00ff";
     } else {
-        barraForca.style.backgroundColor = "#ff0000ff"
+        barraForca.style.backgroundColor = "#ff0000ff";
     }
 }
 
@@ -64,80 +43,82 @@ function diminuirForca() {
 
 function aumentarforca(){
     if (!jogo) {
-     iniciarContagem();
+       
+        jogo = true;
+        tempo = 5;
+        contador.innerHTML = "Tempo: " + tempo + "s";
+        contador.style.background = "linear-gradient(135deg, #33ff00ff, #00a108ff)";
+        
+        intervaloDiminuir = setInterval(diminuirForca, 500); // diminuir forca
+
+        intervalo = setInterval(function() {
+            tempo--;
+            contador.innerHTML = "Tempo: " + tempo + "s";
+
+            if (tempo <= 0) {
+                clearInterval(intervalo);
+                clearInterval(intervaloDiminuir);
+                executarSoco();
+            }
+        }, 1000);
+        
         return;
     }
     
-    if(tempo > 0 && barra < maxBarra){
-        barra++
-        atualizar()
+    if (jogo && tempo > 0 && barra < maxBarra) {
+        barra++;
+        atualizar();
     }
 }
 
 function executarSoco() {
     forcaFinal = barra * 10;
-    contador.innerHTML = "Força final: " + forcaFinal;
-    img_luva.src = "assets/imgs/imgluva.png";
-    img_alvo.src = "assets/imgs/alvo2.png";
-    jogo = false
+    jogo = false;
+
     
-    // Parar de diminuir a força
     clearInterval(intervaloDiminuir);
     
-    // Mostrar resultado
-    mostrarResultado();
     
-    cadastrarBD()
-    janela.style.display='block'
+    img_alvo.src = "assets/imgs/pancada2.png";   
+    contador.innerHTML = "Força final: " + forcaFinal;
+    contador.style.background = "linear-gradient(135deg, #ff0000, #cc0000)";
+    
+    setTimeout(function() {
+        mostrarResultado();
+        janela.style.display = 'block';
+    }, 800);
+    
+    cadastrarBD();
 }
 
 function mostrarResultado() {
     forcaValor.innerHTML = forcaFinal;
     telaResultado.style.display = "flex";
-    geral.style.display= 'none'
-
+    geral.style.display = 'none';
+    contador.style.backgroundColor = "red";
 }
+
 
 function reiniciarJogo() {
-    telaResultado.style.display = "none";
-    iniciarContagem();
-
-}
-
-function iniciar(){
-    // Resetar elementos do jogo
-    img_alvo.src = "assets/imgs/alvo.png";
-    barra = 0 
-    tempo = 5
-    contador.innerHTML = "Tempo: " + tempo + "s";
-    jogo = true
-    atualizar()
-    
-    // Iniciar a diminuição automática da força
-    intervaloDiminuir = setInterval(diminuirForca, 200);
-
-    intervalo = setInterval(function () {
-        tempo--;
-        contador.innerHTML = "Tempo: " + tempo + "s";
-
-        if (tempo <= 0) {
-            clearInterval(intervalo);
-             clearInterval(intervaloDiminuir);
-            executarSoco();
-        }
-    }, 1000); 
-    
+    telaResultado.style.display = 'none';
+    geral.style.display = 'flex';
+    barra = 0;
+    jogo = false;
+    atualizar();
+    contador.innerHTML = "Clique para começar";
+    contador.style.background = "linear-gradient(135deg, #0004f1, #32b9cd)";
+    img_alvo.src = "assets/imgs/pancada.png";
 }
 
 geral.onclick = aumentarforca;
 
 var idUsuario = sessionStorage.ID_USUARIO;
-var fkjogo = 1
+var fkjogo = 1;
 
 function cadastrarBD(){
     var forcaVar = forcaFinal;
     var fkUsuarioVar = idUsuario;
-    var fkjogoVar = fkjogo
+    var fkjogoVar = fkjogo;
     
     fetch("/forca/inserir", {
         method: "POST",
@@ -150,21 +131,4 @@ function cadastrarBD(){
             forcaServer: forcaVar       
         }),
     });
-}
-
-function buscarUltimoIdjogo() {
-    fetch(`/partida/ultimas/${idUsuario}`, { cache: 'no-store' }).then(function (response) {
-        if (response.ok) {
-            response.json().then(function (resposta) {
-                console.log(`Dados recebidos: ${JSON.stringify(resposta)}`);
-                sessionStorage.ID_ULTIMO_JOGO = resposta[0].idPartida
-                console.log(resposta)
-            });
-        } else {
-            console.error('Nenhum dado encontrado ou erro na API');
-        }
-    })
-        .catch(function (error) {
-            console.error(`Erro na obtenção dos dados ${error.message}`);
-        });
 }
